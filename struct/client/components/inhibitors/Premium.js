c3ecb6a6fdd85f3f127cfdc01103eba679160420
nol_an <noooolaaaan@gmail.com>
2019-07-08 03:33:05 -0500

    premium inhibitor fix

--- struct/client/components/inhibitors/Premium.js
+++ struct/client/components/inhibitors/Premium.js
@@ -13,7 +13,7 @@ class Premium extends Inhibitor {
     }
 
     execute(message, command) {
-        if(command.premium && !message.guild.getSetting('premium').value) {
+        if(command.premium && !message.guild._getSetting('premium').value) {
             return super._fail(`The command **${command.moduleResolveable}** can only be run in premium servers.`);
         } else {
             return super._succeed();
